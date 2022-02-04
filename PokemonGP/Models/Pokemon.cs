7447a7316352e10a7757b07e7bf25ed3bab6f35d
alexis <alexisd.horton@gmail.com>
2022-02-04 11:39:52 -0500

    Latest

--- PokemonGP/Models/Pokemon.cs
+++ PokemonGP/Models/Pokemon.cs
@@ -1,12 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace PokemonGP.Models
 {
     public class Pokemon
     {
+        public int height { get; set; }
+        public int weight { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int base_experience { get; set; }
+        public int order { get; set; }
+        public Species species { get; set; }
+        public Sprites sprites { get; set; }
+        public List<Stat> stats { get; set; }
+        public List<Type> types { get; set; }
+    }
+    public class Species
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+    public class Sprites
+    {
+        public string back_default { get; set; }
+        public string front_default { get; set; }
+    }
+    public class Stat
+    {
+        public int base_stat { get; set; }
+        public StatSpecs stat { get; set; }
+    }
+    public class StatSpecs
+    {
+        public string name { get; set; }
+    }
+    public class Type
+    {
+        public TypeSpecs type { get; set; }
+    }
+    public class TypeSpecs
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
 
+    public class PokemonAPI
+    {
+        private static HttpClient _realClient = null;
+        public static HttpClient MyHttp
+        {
+            get
+            {
+                if (_realClient == null)
+                {
+                    _realClient = new HttpClient();
+                    _realClient.BaseAddress = new Uri("https://pokeapi.co/api/v2/"); // ADD YOUR OWN BASE ADDRESS HERE
+                }
+                return _realClient;
+            }
+        }
     }
 }
---
+++ PokemonGP/Models/UserLogin.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokemonGP.Models
+{
+    public class UserLogin
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+
+    public class MyTeam
+    {
+        public int id { get; set; }
+        public int level { get; set; }
+        public int experience { get; set; }
+        public int userid { get; set; }
+        public int teampos { get; set; }
+
+    }
+
+    public class UserDB
+    {
+        public static bool AddUser(UserLogin user)
+        {
+            using (PokemonContext ctx = new PokemonContext())
+            {
+                ctx.UserStorage.Add(user);
+                ctx.SaveChanges();
+            }
+            return true;
+        }
+
+        public static bool DeleteUser(int userid)
+        {
+            using (PokemonContext ctx = new PokemonContext())
+            {
+                UserLogin theUser = new UserLogin();
+                theUser.id = userid;
+                ctx.Remove(theUser);
+                ctx.SaveChanges();
+                return true;
+            }
+        }
+    }
+
+    public class PokemonContext : DbContext
+    {
+        public DbSet<UserLogin> UserStorage { get; set; }
+        public DbSet<MyTeam> MyTeamStorage { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=pokemonuserlogin;Integrated Security=SSPI;");
+            // Or For username/password, use the following:
+            // optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=efconsole1;User Id=sa;Password=abc123;");
+        }
+    }
+}
